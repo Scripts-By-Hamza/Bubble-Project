be751a7462826508ce6a4da38d56ed729d317f8d
Muhammad Hamza <rajpoothamza2412@gmail.com>
2025-05-15 11:35:13 +0500

    Completed the project
    I enjoyed a lot to create this wonder full project

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
+var timer = 30;
+var hitRandom;
+var score = 0;
+var timerint;
+var correctClick = score / 10;
+
+function increaseScore() {
+  score += 10;
+  document.querySelector(".score-val").textContent = score;
+}
+
+function getNewHit() {
+  hitRandom = Math.floor(Math.random() * 10);
+  document.querySelector(".hit-val").textContent = hitRandom;
+}
+
 function displayBubbles() {
   var bubbles = "";
-
   for (var i = 0; i <= 167; i++) {
     const random = Math.floor(Math.random() * 10);
-    bubbles += `<div id="bubble"> ${random} </div>`;
+    bubbles += `<div id="bubble"> ${random} </div>`; // changed from id to class
   }
-
   document.querySelector("#play-area").innerHTML = bubbles;
 }
 
-var timer = 60;
 function runtimer() {
-  var timerint = setInterval(function () {
+  timerint = setInterval(function () {
     if (timer > 0) {
       timer--;
       document.querySelector(".timer-start").textContent = timer;
     } else {
       clearInterval(timerint);
+      document.querySelector(
+        "#play-area"
+      ).innerHTML = `<h1>Time's Up! Your Score is ${score}. <br> Refresh To Start Again. </h1>`;
     }
   }, 1000);
 }
-runtimer();
+
+document.querySelector("#play-area").addEventListener("click", function (dets) {
+  var clickedNum = parseInt(dets.target.textContent);
+  if (clickedNum === hitRandom) {
+    increaseScore();
+    displayBubbles();
+    getNewHit();
+  } else {
+    clearInterval(timerint);
+    document.querySelector(
+      "#play-area"
+    ).innerHTML = `<h1>Game Over 😖  <br> You Clicked ${correctClick} bubbles so, <br> Your Score is ${score}. <br> Refresh To Start Again. </h1>`;
+    document.querySelector(".timer-start").textContent = 0;
+  }
+});
+
+getNewHit();
 displayBubbles();
+runtimer();
